4bf64031a8ae4d3cff68a037e0611dbc13f867c5
dunksjunk <richdunkel@gmail.com>
2015-07-27 21:53:20 -0500

    cleanup and fix

--- src/ADAuthUserProvider.php
+++ src/ADAuthUserProvider.php
@@ -31,26 +31,29 @@ class ADAuthUserProvider implements UserProvider {
    *
    * @var string
    */
-   
+
    /**
    * Server Connection
    *
    * @var LDAP link identifier
    */
-   
+
   protected $adConnection;
-  
+
+
   /**
    * Pull up a new AD User Provider
    * @param none
-   */  
+   */
   public function __construct() {
     $this->fetchConfig();
   }
 
+
   public function retrieveById( $identifier ){
     return $this->createModel()->newQuery()->find( $identifier );
   }
+
 
   public function retrieveByToken( $identifier, $token ){
     $model = $this->createModel();
@@ -60,45 +63,50 @@ class ADAuthUserProvider implements UserProvider {
         ->where( $model->getRememberTokenName(), $token )
         ->first();
   }
-  
+
+
   public function updateRememberToken( Authenticatable $user, $token ){
     $user->setRememberToken( $token );
-    $user->save();    
+    $user->save();
   }
-  
+
+
   public function retrieveByCredentials( array $credentials ){}
-  
+
+
   public function validateCredentials( Authenticatable $user, array $credentials ){
-    if ( !$this->adConnection = serverConnect() ) { 
-      throw new Exception( 'Can not connect to Active Directory Server.' ); 
+    if ( !$this->adConnection = serverConnect() ) {
+      throw new Exception( 'Can not connect to Active Directory Server.' );
     }
-    
+
     // handle the credentials better
     $adResult = ldap_bind( $this->adConnection, $credentials[0], $credentials[1] );
     ldap_close( $this->adConnection );
     return $adResult;
   }
-  
+
+
   private function fetchConfig() {
-    $this->adAuthServer = \Config::get( 'adauth:adAuthServer' );
+    $this->adAuthServer = \Config::get( 'adauth.adAuthServer' );
     $this->adAuthPort = \Config::get( 'adauth.adAuthPort' );
     $this->adAuthShortDomain = \Config::get( 'adauth.adAuthShortDomain' );
-    $this->adAuthModel = \Config::get( 'auth.model' );    
+    $this->adAuthModel = \Config::get( 'auth.model' );
   }
-  
+
+
   private function serverConnect() {
     $adConnectionString = 'ldap://';
-    $adConnectionString .= $this->adAuthServer . ':' . $this->adAuthPort . '/'; 
-    
+    $adConnectionString .= $this->adAuthServer . ':' . $this->adAuthPort . '/';
+
     $this->adConnection = ldap_connect( $adConnectionString );
 
-    ldap_set_option($this->adConnection, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option($con, LDAP_OPT_REFERRALS, 0);
+    ldap_set_option( $this->adConnection, LDAP_OPT_PROTOCOL_VERSION, 3 );
+    ldap_set_option( $this->adConnection, LDAP_OPT_REFERRALS, 0 );
 
     return $this->adConnection;
   }
-  
-  
+
+
   public function createModel() {
     $class = '\\' . ltrim( $this->model, '\\' );
     return new $class;
